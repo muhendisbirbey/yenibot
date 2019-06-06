aa5dd749fda0e951bf748b8da682570e3da5f4f7
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-06-06 20:02:14 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var oyun = [
+        "YouTube > Mühendis Bey ile Mühendis Hanım ",
+        "Instagram > @muhendisbeyilemuhendishanim ",
+	"DLive.tv/muhendisbeyilhanim ",
+	"Mühendis BOT  " +bot.guilds.size+ "  sunucuda  " + bot.users.size +" kullanıcıya hizmet veriyor!",
+	"Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :)"
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/muhendisbeymuhendishanim");
+        }, 2 * 2500);
+        
+bot.login(process.env.BOT_TOKEN);
